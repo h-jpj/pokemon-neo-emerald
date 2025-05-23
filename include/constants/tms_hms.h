5a19247aa8da70d5213ec91fbef7c38cf3447531
hj <harveyjay@tuta.io>
2025-05-24 00:56:04 +0100

    v2

---
+++ include/constants/tms_hms.h
@@ -0,0 +1,95 @@
+#ifndef GUARD_CONSTANTS_TMS_HMS_H
+#define GUARD_CONSTANTS_TMS_HMS_H
+
+#define FOREACH_TM(F) \
+    F(FOCUS_PUNCH) \
+    F(DRAGON_CLAW) \
+    F(SCALD) \
+    F(CALM_MIND) \
+    F(ROAR) \
+    F(TOXIC) \
+    F(SNOWSCAPE) \
+    F(BULK_UP) \
+    F(BULLET_SEED) \
+    F(HIDDEN_POWER) \
+    F(SUNNY_DAY) \
+    F(TAUNT) \
+    F(ICE_BEAM) \
+    F(BLIZZARD) \
+    F(HYPER_BEAM) \
+    F(LIGHT_SCREEN) \
+    F(PROTECT) \
+    F(RAIN_DANCE) \
+    F(GIGA_DRAIN) \
+    F(SAFEGUARD) \
+    F(FRUSTRATION) \
+    F(SOLAR_BEAM) \
+    F(IRON_TAIL) \
+    F(THUNDERBOLT) \
+    F(THUNDER) \
+    F(EARTHQUAKE) \
+    F(RETURN) \
+    F(DIG) \
+    F(PSYCHIC) \
+    F(SHADOW_BALL) \
+    F(BRICK_BREAK) \
+    F(DOUBLE_TEAM) \
+    F(REFLECT) \
+    F(VOLT_SWITCH) \
+    F(FLAMETHROWER) \
+    F(SLUDGE_BOMB) \
+    F(SANDSTORM) \
+    F(FIRE_BLAST) \
+    F(ROCK_TOMB) \
+    F(ROOST) \
+    F(TORMENT) \
+    F(FACADE) \
+    F(SECRET_POWER) \
+    F(REST) \
+    F(ATTRACT) \
+    F(THIEF) \
+    F(STEEL_WING) \
+    F(SKILL_SWAP) \
+    F(SNATCH) \
+    F(OVERHEAT) \
+    F(THUNDER_WAVE) \
+    F(CONFUSE_RAY) \
+    F(TRICK_ROOM) \
+    F(TIDY_UP) \
+    F(HONE_CLAWS) \
+    F(U_TURN) \
+    F(FOCUS_BLAST) \
+    F(DRAIN_PUNCH) \
+    F(FOUL_PLAY) \
+    F(SMART_STRIKE) \
+    F(SWORDS_DANCE) \
+    F(FROST_BREATH) \
+    F(LEECH_LIFE) \
+    F(DAZZLING_GLEAM) \
+    F(ENERGY_BALL) \
+    F(PLAY_ROUGH) \
+    F(DRILL_RUN) \
+    F(STONE_EDGE) \
+    F(POISON_JAB) \
+    F(DARK_PULSE) \
+    F(ROCK_SLIDE) \
+    F(DRAGON_PULSE) \
+    F(WEATHER_BALL) \
+    F(RAPID_SPIN) \
+    F(STEALTH_ROCK)
+
+#define FOREACH_HM(F) \
+    F(CUT) \
+    F(FLY) \
+    F(SURF) \
+    F(STRENGTH) \
+    F(FLASH) \
+    F(ROCK_SMASH) \
+    F(WATERFALL) \
+    F(DIVE)
+
+#define FOREACH_TMHM(F) \
+    FOREACH_TM(F) \
+    FOREACH_HM(F)
+
+#endif
---
+++ src/data/battle_partners.h
@@ -0,0 +1,111 @@
+//
+// DO NOT MODIFY THIS FILE! It is auto-generated from src/data/battle_partners.party
+//
+// If you want to modify this file set COMPETITIVE_PARTY_SYNTAX to FALSE
+// in include/config/general.h and remove this notice.
+// Use sed -i '/^#line/d' 'src/data/battle_partners.h' to remove #line markers.
+//
+
+#line 1 "src/data/battle_partners.party"
+
+#line 1
+    [DIFFICULTY_NORMAL][PARTNER_NONE] =
+    {
+#line 3
+        .trainerClass = TRAINER_CLASS_PKMN_TRAINER_1,
+#line 4
+        .trainerPic = TRAINER_BACK_PIC_BRENDAN,
+        .encounterMusic_gender =
+#line 6
+            TRAINER_ENCOUNTER_MUSIC_MALE,
+        .partySize = 0,
+        .party = (const struct TrainerMon[])
+        {
+        },
+    },
+#line 8
+    [DIFFICULTY_NORMAL][PARTNER_STEVEN] =
+    {
+#line 9
+        .trainerName = _("STEVEN"),
+#line 10
+        .trainerClass = TRAINER_CLASS_RIVAL,
+#line 11
+        .trainerPic = TRAINER_BACK_PIC_STEVEN,
+        .encounterMusic_gender =
+#line 13
+            TRAINER_ENCOUNTER_MUSIC_MALE,
+        .partySize = 3,
+        .party = (const struct TrainerMon[])
+        {
+            {
+#line 15
+            .species = SPECIES_METAGROSS,
+            .gender = TRAINER_MON_RANDOM_GENDER,
+#line 15
+            .heldItem = ITEM_METAGROSSITE,
+#line 19
+            .ev = TRAINER_PARTY_EVS(0, 252, 252, 0, 6, 0),
+#line 18
+            .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
+#line 17
+            .lvl = 64,
+#line 16
+            .nature = NATURE_BRAVE,
+            .dynamaxLevel = MAX_DYNAMAX_LEVEL,
+            .moves = {
+#line 20
+                MOVE_LIGHT_SCREEN,
+                MOVE_PSYCHIC,
+                MOVE_REFLECT,
+                MOVE_METAL_CLAW,
+            },
+            },
+            {
+#line 25
+            .species = SPECIES_SKARMORY,
+            .gender = TRAINER_MON_RANDOM_GENDER,
+#line 25
+            .heldItem = ITEM_LEFTOVERS,
+#line 29
+            .ev = TRAINER_PARTY_EVS(252, 0, 0, 0, 6, 252),
+#line 28
+            .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
+#line 27
+            .lvl = 62,
+#line 26
+            .nature = NATURE_IMPISH,
+            .dynamaxLevel = MAX_DYNAMAX_LEVEL,
+            .moves = {
+#line 30
+                MOVE_TOXIC,
+                MOVE_AERIAL_ACE,
+                MOVE_PROTECT,
+                MOVE_STEEL_WING,
+            },
+            },
+            {
+#line 35
+            .species = SPECIES_AGGRON,
+            .gender = TRAINER_MON_RANDOM_GENDER,
+#line 40
+            .ev = TRAINER_PARTY_EVS(0, 252, 0, 0, 252, 6),
+#line 39
+            .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
+#line 38
+            .ability = ABILITY_FILTER,
+#line 37
+            .lvl = 64,
+#line 36
+            .nature = NATURE_ADAMANT,
+            .dynamaxLevel = MAX_DYNAMAX_LEVEL,
+            .moves = {
+#line 41
+                MOVE_THUNDER,
+                MOVE_PROTECT,
+                MOVE_SOLAR_BEAM,
+                MOVE_DRAGON_CLAW,
+            },
+            },
+        },
+    },
